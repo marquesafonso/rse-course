c4eafc5d21974bd165077b33ee7a9bd6ad838d68
marquesafonso <afonso.cunha.marques@gmail.com>
2022-01-21 17:58:12 +0000

    add day5 materials

--- module05_testing_your_code/diffusion/model.py
+++ module05_testing_your_code/diffusion/model.py
@@ -1,28 +1,23 @@
-"""Simplistic 1-dimensional diffusion model."""
-from numpy import array, any, sum
-
-
-def energy(density):
+def energy(density, coeff=1.0):
     """Energy associated with the diffusion model
-    :Parameters:
-      density: array of positive integers
-         Number of particles at each position i in the array/geometry
-    """
 
-    # Make sure input is an numpy array
+    Parameters
+    ----------
+
+    density: array of positive integers
+        Number of particles at each position i in the array
+    coeff: float
+        Diffusion coefficient.
+    """
+    from numpy import array,sum, int32
+
     density = array(density)
 
-    # ...of the right kind (integer). Unless it is zero length,
-    #    in which case type does not matter.
-
-    if density.dtype.kind != "i" and len(density) > 0:
-        raise TypeError("Density should be a array of *integers*.")
-    # and the right values (positive or null)
-    if any(density < 0):
-        raise ValueError("Density should be an array of *positive* integers.")
-    if density.ndim != 1:
-        raise ValueError(
-            "Density should be an a *1-dimensional*" + "array of positive integers."
-        )
-
-    return sum(density * (density - 1))
+    if density.dtype != int32 and len(density) <= 0:
+        raise TypeError("Inputs should be integers and array should be non-empty")
+    elif any(density < 0):
+        raise ValueError("Inputs should be positive")
+    elif len(density.shape) != 1:
+        raise TypeError("Density array should be 1D")
+    else:
+        return sum(density * (density -1))
